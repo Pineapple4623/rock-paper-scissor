2947f932f08721044a921d65575d4fec7636c490
Pineapple4623 <areebkhan69@outlook.com>
2025-06-25 23:25:54 +0500

    Modify the logic

--- game.js
+++ game.js
@@ -1,7 +1,8 @@
 // test = Math.floor(Math.random() * 3) +1
 
 // console.log(test)
-
+let humanScore =0;
+let compScore = 0;
 function getComputerChoice(){
     num = Math.floor(Math.random() * 3) +1
     let choice;
@@ -21,44 +22,51 @@ function getHumanChoice(){
      return prompt("Type for Scissors, Type for Rock, Type for Paper").toLowerCase();
 }
 
-let humanScore =0;
-let compScore = 0;
+
 
 function playRound(humanChoice, computerChoice) {
   // your code here!
   if (humanChoice == "scissors" && computerChoice == "paper"){
-    console.log("You win! Scissors beat Paper")
     humanScore++;
+    console.log("You win! Scissors beat Paper")
+    console.log("human Score: ", humanScore, "ComputerScore: ", compScore)
 
   }
   else if (humanChoice == "rock" && computerChoice == "scissors")
   {
-    console.log("You win! Rock beats Scissor")
     humanScore++;
+    console.log("You win! Rock beats Scissor")
+    console.log("human Score: ", humanScore, "ComputerScore: ", compScore)
   }
   else if(humanChoice == "paper" && computerChoice == "rock"){
+    humanScore++;
     console.log("You win! Paper beats Rock")
     humanScore++;
+    console.log("human Score: ", humanScore, "ComputerScore: ", compScore)
   }
   else if(humanChoice == "scissors" && computerChoice == "rock" )
   {
-    console.log("You lost! Scissors lost to Rock.")
     compScore++;
+    console.log("You lost! Scissors lost to Rock.")
+    console.log("human Score: ", humanScore, "ComputerScore: ", compScore)
   }
   else if(humanChoice == "rock" && computerChoice == "paper" )
   {
-    console.log("You lost! Rock lost to Paper.")
     compScore++;
+    console.log("You lost! Rock lost to Paper.")
+    console.log("human Score: ", humanScore, "ComputerScore: ", compScore)
   }
   else if(humanChoice == "paper" && computerChoice == "scissors" )
   {
-    console.log("You lost! Paper lost to Scissors.")
     compScore++;
+    console.log("You lost! Paper lost to Scissors.")
+    console.log("human Score: ", humanScore, "ComputerScore: ", compScore)
   }
   else if (humanChoice == computerChoice) {
-    console.log("It's a tie! You and Computer guessed the same.")
     humanScore++;
     compScore++;
+    console.log("It's a tie! You and Computer guessed the same.")
+    console.log("human Score: ", humanScore, "ComputerScore: ", compScore)
   }
 }
 function playGame(){
@@ -68,6 +76,16 @@ function playGame(){
 
         playRound(humanSelection, computerSelection);
     }
-}
+    if(humanScore >compScore){
+        console.log("You Win!")
+        console.log("human Score: ", humanScore, "ComputerScore: ", compScore)
 
+    }
+    else {
+        console.log("Computer wins!")
+        console.log("human Score: ", humanScore, "ComputerScore: ", compScore)
+    }
+
+}
+playGame()
 
